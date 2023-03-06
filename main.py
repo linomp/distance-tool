7f6f033557804d7d1a93336cdb9961766233b89b
LinoMediavilla <lino.mediavilla@linksfoundation.com>
2023-03-06 12:11:51 +0100

    add API key input field to UI & Google API improve error handling

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import streamlit as st
 
 from utils import process_input_file
 
-# create a file upload button
-uploaded_file = st.file_uploader("Choose a file")
+# Create an API key input field:
+api_key = st.text_input("Enter your Google API key", type="password")
+
+# create a file upload button (disabled until an API key is entered)
+uploaded_file = st.file_uploader("Upload an excel file", type=['xlsx'], disabled=not api_key)
 
 # if the user uploaded a file
 if uploaded_file is not None:
     if uploaded_file.name.split('.')[-1] != 'xlsx':
         st.error('Please upload an excel file')
     else:
-        # process the file
-        file, filename = process_input_file(uploaded_file)
+        try:
+            # process the file
+            file, filename = process_input_file(uploaded_file, api_key=api_key, standalone_mode=False)
 
-        # display a success message
-        st.success('File processed successfully')
+            # display a success message
+            st.success('File processed successfully')
 
-        # display a download button
-        st.download_button("Download processed file", file_name=filename, data=file,
-                           mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+            # display a download button
+            st.download_button("Download processed file", file_name=filename, data=file,
+                               mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        except Exception as e:
+            st.error(e)
--- utils.py
+++ utils.py
@@ -1,17 +1,22 @@
-import os
 import time
+import urllib.parse
 from io import BytesIO
 
-import requests
-import urllib.parse
 import pandas as pd
+import requests
+
+
+class GoogleMapsRequestException(Exception):
+    pass
 
 
 def get_distance_in_km(source: str, dest: str, api_key: str):
+    # Source: https://developers.google.com/maps/documentation/distance-matrix/distance-matrix#maps_http_distancematrix_latlng-py
+
     source = urllib.parse.quote(source)
     dest = urllib.parse.quote(dest)
 
-    url = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={source}&destinations={dest}&units=metric&key={api_key}"
+    url = f"https://maps.googleapis.com/maps/api/distancematrix/json?mode=driving&origins={source}&destinations={dest}&units=metric&key={api_key}"
 
     payload = {}
     headers = {}
@@ -20,42 +25,33 @@ def get_distance_in_km(source: str, dest: str, api_key: str):
 
     response = response.json()
 
-    distance = response['rows'][0]['elements'][0]['distance']['text']
+    if "error_message" in response:
+        raise GoogleMapsRequestException(response["error_message"])
 
-    # remove "km" from the string
-    distance = distance[:-2]
+    distance: float = response['rows'][0]['elements'][0]['distance']['value']
+
+    # convert from m to km
+    distance /= 1000
 
     return distance
 
 
-def load_api_key():
-    key = os.getenv('GOOGLE_MAPS_API_KEY', None)
-    if not key:
-        with open('.env', 'r') as f:
-            key = f.read().split('=')[1]
-
-            if not key:
-                raise Exception('No API key found')
-    return key
-
-
-def process_input_file(input_file: str | BytesIO, api_key: str | None = None, save_local: bool = False):
+def process_input_file(input_file: str | BytesIO, api_key: str, standalone_mode: bool = False):
     """
     Function that reads the 2 first columns of an excel file, and calls the get_distance_in_km function, then writes the result in a new column, and saves the file as a new excel file
     """
-
-    if api_key is None:
-        api_key = load_api_key()
 
     df = pd.read_excel(input_file, engine='openpyxl', header=None)
 
     df = df.iloc[:, :2]
 
     for index, row in df.iterrows():
-        time.sleep(0.1)
+        time.sleep(0.005)
         try:
             distance = get_distance_in_km(row[0], row[1], api_key)
-        except:
+        except GoogleMapsRequestException as e:
+            raise e
+        except Exception:
             distance = 'error'
 
         # write distance in 3rd column
@@ -64,7 +60,7 @@ def process_input_file(input_file: str | BytesIO, api_key: str | None = None, sa
     # output_file_name is the same as input but "with_distances" and the current time in hh:mm:ss
     output_file_name = 'computed_distances_' + time.strftime("%H_%M_%S") + '.xlsx'
 
-    if save_local:
+    if standalone_mode:
         df.to_excel(output_file_name, index=False, header=["Source", "Destination", "Distance (km)"])
     else:
         return to_excel(df, header=["Source", "Destination", "Distance (km)"]), output_file_name
@@ -79,5 +75,14 @@ def to_excel(df, header=None):
     return processed_data
 
 
+def load_api_key():
+    with open('.env', 'r') as f:
+        key = f.read().split('=')[1]
+        if not key:
+            raise Exception('No API key found')
+    return key
+
+
 if __name__ == '__main__':
-    process_input_file("input.xlsx", save_local=True)
+    api_key = load_api_key()
+    process_input_file("input.xlsx", api_key=api_key, standalone_mode=True)
